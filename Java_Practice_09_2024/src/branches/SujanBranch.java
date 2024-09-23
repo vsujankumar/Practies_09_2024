6c01d3b7db752fccb5cc46ba27dd85254950f7e8
unknown <vsujankumarreddy@gmail.com>
2024-09-24 00:20:34 +0530

    project new changes

---
+++ Java_Practice_09_2024/src/branches/SujanBranch.java
@@ -0,0 +1,17 @@
+package branches;
+
+public class SujanBranch {
+
+	public static void main(String[] args) {
+
+		System.out.println("master");
+
+		System.out.println("student1");
+
+		System.out.println("student2");
+
+		System.out.println("sujan");
+	
+
+	}
+}
